4d8e844894130fe973bbcf110d0ed77d4633c7e2
Essential Randomness <essentialrandomn3ss@gmail.com>
2024-11-21 13:35:20 -0800

    get PWA share to work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,47 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import AstroPWA from "@vite-pwa/astro";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  integrations: [
+    AstroPWA({
+      devOptions: {
+        enabled: true,
+      },
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Fanworks Labeler",
+        short_name: "Fanworks Labeler",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa/192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa/512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa/512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+        share_target: {
+          action: "/share-target/",
+          method: "GET",
+          params: {
+            title: "title",
+            text: "text",
+            url: "url",
+          },
+        },
+      },
+    }),
+  ],
+});
