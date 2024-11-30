68531747e3b90bbebb4fd81c3559b956a8e5fa5f
Essential Randomness <essentialrandomn3ss@gmail.com>
2024-11-30 15:44:22 -0800

    add label configuration for astro site

---
+++ labels.ts
@@ -0,0 +1,28 @@
+export default {
+  labels: [
+    {
+      value: "fanfic",
+      description: "This post has been marked as a fanfiction",
+    },
+    {
+      value: "shitpost",
+      description: "This post has been marked as a shitpost",
+    },
+    {
+      value: "art",
+      description: "This post has been marked as an artwork",
+    },
+    {
+      value: "meta",
+      description: "This post has been marked as meta",
+    },
+    {
+      value: "headcanon",
+      description: "This post has been marked as a headcanon",
+    },
+    {
+      value: "threadfic",
+      description: "This post has been marked as a threadfic",
+    },
+  ],
+};
--- src/actions/labelPost.ts
+++ src/actions/labelPost.ts
@@ -1,55 +1,65 @@
 import { ActionError, defineAction } from "astro:actions";
 import { z } from "astro:schema";
 import { getBskyAgent } from "../auth/client";
-
+import labelsConfig from "../../labels";
 
 export const labelPost = defineAction({
-    accept: "form",
-    input: z.object({
-      post: z.string().url(),
-      labels: z.string().array(),
-    }),
-    handler: async (input, ctx) => {
-      const did = ctx.locals.session?.userDid;
-      if (!did) {
-        throw new ActionError({
-          code: "UNAUTHORIZED",
-          message: "No did found for user",
-        });
-      }
-      const agent = await getBskyAgent({ did });
-      if (!agent?.did) {
-        throw new ActionError({
-          code: "UNAUTHORIZED",
-          message: "Your BlueSky login expired",
-        });
-      }
+  accept: "form",
+  input: z.object({
+    post: z.string().url(),
+    labels: z.string().array(),
+  }),
+  handler: async (input, ctx) => {
+    const did = ctx.locals.session?.userDid;
+    if (!did) {
+      throw new ActionError({
+        code: "UNAUTHORIZED",
+        message: "No did found for user",
+      });
+    }
+    const agent = await getBskyAgent({ did });
+    if (!agent?.did) {
+      throw new ActionError({
+        code: "UNAUTHORIZED",
+        message: "Your BlueSky login expired",
+      });
+    }
+    const hasExtraLabels = input.labels.some(
+      (label) =>
+        !labelsConfig.labels.find((configLabel) => label == configLabel.value)
+    );
+    if (hasExtraLabels) {
+      throw new ActionError({
+        code: "BAD_REQUEST",
+        message: "You're trying to set a label that does not exist",
+      });
+    }
 
-      const url = new URL(input.post);
-      const key = url.pathname.substring(url.pathname.lastIndexOf("/") + 1);
-      try {
-        const post = await agent.getPost({
-          repo: agent.did,
-          rkey: key,
-        });
-        const request = await fetch("http://127.0.0.1:14832/labels", {
-          method: "PUT",
-          body: JSON.stringify({
-            at_url: post.uri,
-            labels: input.labels,
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
+    const url = new URL(input.post);
+    const key = url.pathname.substring(url.pathname.lastIndexOf("/") + 1);
+    try {
+      const post = await agent.getPost({
+        repo: agent.did,
+        rkey: key,
+      });
+      const request = await fetch("http://127.0.0.1:14832/labels", {
+        method: "PUT",
+        body: JSON.stringify({
+          at_url: post.uri,
+          labels: input.labels,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
-        const addLabels = await request.json();
-        return `thank you for adding ${addLabels.added} and removing ${addLabels.removed} to ${input.post}`;
-      } catch (e) {
-        throw new ActionError({
-          code: "FORBIDDEN",
-          message: "Post does not exist or is not yours",
-        });
-      }
-    },
-  });
+      const addLabels = await request.json();
+      return `thank you for adding ${addLabels.added} and removing ${addLabels.removed} to ${input.post}`;
+    } catch (e) {
+      throw new ActionError({
+        code: "FORBIDDEN",
+        message: "Post does not exist or is not yours",
+      });
+    }
+  },
+});
