63cd5c2b72e0e56ba4540218be4f5ee82ca8f25b
Essential Randomness <essentialrandomn3ss@gmail.com>
2024-12-05 16:55:11 -0800

    add all-labels feed

--- feed-server/src/algos/index.ts
+++ feed-server/src/algos/index.ts
@@ -3,12 +3,12 @@ import {
   QueryParams,
   OutputSchema as AlgoOutput,
 } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
-import * as whatsAlf from './whats-alf'
+import * as allLabels from './all-labels'
 
 type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
 
 const algos: Record<string, AlgoHandler> = {
-  [whatsAlf.shortname]: whatsAlf.handler,
+  [allLabels.shortname]: allLabels.handler,
 }
 
 export default algos
