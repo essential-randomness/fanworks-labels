33341d3fed197a5763b323cc74f41d8350a3770a
Essential Randomness <essentialrandomn3ss@gmail.com>
2024-12-12 17:30:30 -0800

    add two feeds for specific label

--- feed-server/src/algos/index.ts
+++ feed-server/src/algos/index.ts
@@ -4,11 +4,15 @@ import {
   OutputSchema as AlgoOutput,
 } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
 import * as allLabels from './all-labels'
+import * as byLabel from './by-label'
 
 type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
 
 const algos: Record<string, AlgoHandler> = {
   [allLabels.shortname]: allLabels.handler,
+  fanfic: byLabel.handler,
+  shitpost: byLabel.handler,
+  art: byLabel.handler,
 }
 
 export default algos
