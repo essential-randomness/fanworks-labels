50ae6ec13ba8cc3c8b50e161970b1194cbeb5be5
Essential Randomness <essential.randomn3ss@gmail.com>
2024-12-17 01:13:10 +0000

    allow feed by label

--- feed-server/src/algos/by-label.ts
+++ feed-server/src/algos/by-label.ts
@@ -1,18 +1,19 @@
 import { QueryParams } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
 import { AppContext } from '../config'
+import Database from "better-sqlite3";
+const db = new Database("../labels-server/labels.db");
 
 export const handler = async (ctx: AppContext, params: QueryParams) => {
   const requestedFeed = params.feed
-  const label = params.feed.substring(params.feed.lastIndexOf('/'))
-  console.log('feed requested:', requestedFeed)
-  console.log('label requested:', label)
+  const label = params.feed.substring(params.feed.lastIndexOf('/') + 1)
+  // console.log('feed requested:', requestedFeed)
+  // console.log('label requested:', label)
 
   let builder = ctx.db
     .selectFrom('post')
     .selectAll()
     .orderBy('indexedAt', 'desc')
-    .orderBy('cid', 'desc')
-    .limit(params.limit)
+    .orderBy('cid', 'desc');
 
   if (params.cursor) {
     const timeStr = new Date(parseInt(params.cursor, 10)).toISOString()
@@ -20,12 +21,19 @@ export const handler = async (ctx: AppContext, params: QueryParams) => {
   }
   const res = await builder.execute()
 
-  const feed = res.map((row) => ({
+  const labelsQuery = db.prepare(`select labels.uri, labels.cts, neg from (select uri, count(*), max(cts) as m_cts from labels where val = ? group by uri order by cts desc) as max_op left join labels on max_op.m_cts = labels.cts and max_op.uri = labels.uri;`);
+  const labelsData = labelsQuery.all(label);
+
+  const activeLabelsUris = labelsData.filter((v: any) => !v.neg).map((v:any) => v.uri);
+  const postsWithLabel = res.filter(row => activeLabelsUris.find(uri => uri == row.uri));
+  const postsInFeed = postsWithLabel.slice(0, params.limit);
+
+  const feed = postsInFeed.map((row) => ({
     post: row.uri,
   }))
 
   let cursor: string | undefined
-  const last = res.at(-1)
+  const last = postsInFeed.at(-1)
   if (last) {
     cursor = new Date(last.indexedAt).getTime().toString(10)
   }
