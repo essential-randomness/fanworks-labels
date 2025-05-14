44d8270db81862ca7f7753ce2465d5c6731d0e10
xxwhirlpool <witnesslachesis@disroot.org>
2025-05-14 03:48:54 +0000

    i am going to eat drywall

--- packages/x86_64-linux/labels-server/default.nix
+++ packages/x86_64-linux/labels-server/default.nix
@@ -4,23 +4,35 @@
   buildNpmPackage,
   nodejs_22,
   makeWrapper,
+  importNpmLock,
   ...
 }:  let
-  package-json = lib.importJSON (lib.snowfall.fs.get-file "./labels-server/package.json");
+    package-json = lib.importJSON (lib.snowfall.fs.get-file "./labels-server/package.json");
+    labels = lib.snowfall.fs.get-file "./labels.ts";
 in
   buildNpmPackage {
     pname = "fanworks-labels-server";
-    inherit (package-json) version;
+	inherit (package-json) version;
+	
+    src = lib.snowfall.fs.get-file "./labels-server";
 
-    src = lib.snowfall.fs.get-file "/labels-server/";
-    
-    npmDepsHash = "sha256-EIHnbYWrSkRiE5s1xpu4rK3rzefDtWwCg4XiGhfvf4Y=";
+    npmDeps = importNpmLock {
+    	npmRoot = lib.snowfall.fs.get-file "./labels-server";
+    };
+
+    npmConfigHook = importNpmLock.npmConfigHook;
+
+    npmFlags = [ "--ignore-scripts" ];
 
     nodejs = nodejs_22;
 
     dontNpmBuild = true;
 
     nativeBuildInputs = [makeWrapper];
+
+	postUnpack = ''
+		cp --no-preserve=mode ${labels} $sourceRoot/src/labels.ts
+	'';
 
     postInstall = ''
       makeWrapper ${nodejs_22}/bin/node $out/bin/fanworks-labels-server --add-flags $out/lib/node_modules/fanworks-labels-server/node_modules/.bin/tsx --add-flags $out/lib/node_modules/fanworks-labels-server/src/index.ts
