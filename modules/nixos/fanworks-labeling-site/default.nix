422b9c46d0bd17fd113b656d803351dbe0a3d9a4
xxwhirlpool <witnesslachesis@disroot.org>
2025-07-18 23:03:19 +0000

    labeling site package idk if it works

---
+++ modules/nixos/fanworks-labeling-site/default.nix
@@ -0,0 +1,104 @@
+{
+    lib,
+    pkgs,
+    inputs,
+
+    namespace, # The namespace used for your flake, defaulting to "internal" if not set.
+    system, # The system architecture for this host (eg. `x86_64-linux`).
+    target, # The Snowfall Lib target for this system (eg. `x86_64-iso`).
+    format, # A normalized name for the system target (eg. `iso`).
+    virtual, # A boolean to determine whether this system is a virtual target using nixos-generators.
+    systems, # An attribute map of your defined hosts.
+
+    config,
+    ...
+}:
+let
+    inherit (builtins) toString;
+    inherit (lib) types mkIf mkOption mkDefault;
+    inherit (lib) optional optionals optionalAttrs optionalString;
+    cfg = config.services.fanworks-labeling-site;
+in {
+    options.services.fanworks-labeling-site = {
+        enable =
+            lib.mkEnableOption "Fanworks feeds labeling site"; 
+
+        host = mkOption {
+            type = types.str;
+            description = "The public host name to serve.";
+            example = "labelfanworks.fujocoded.com/";
+        };
+
+        port = mkOption {
+            type = types.port;
+            default = 14831;
+            description = "The port the labeling site should listen on.";
+        };
+
+        user = mkOption {
+            type = types.str;
+            default = "fanworks-labeling-site";
+            description = "User under which fanworks-labeling-site is ran.";
+        };
+
+        group = mkOption {
+            type = types.str;
+            default = "fanworks-labeling-site";
+            description = "Group under which fanworks-labeling-site is ran.";
+        };
+
+        package = mkOption {
+            type = types.package;
+            default = pkgs.${namespace}.labeling-site;
+            description = "The labeling site package to run";
+        };
+    };
+  
+    config = mkIf cfg.enable {
+        users = {
+            users = optionalAttrs (cfg.user == "fanworks-labeling-site") {
+                fanworks-labeling-server = {
+                    group = cfg.group;
+                    home = cfg.stateDir;
+                    isSystemUser = true;
+                };
+            };
+
+            groups =
+                optionalAttrs (cfg.group == "fanworks-labeling-site") { fanworks-labeling-site = { }; };
+        };
+
+        systemd.services.fanworks-labeling-site = {
+            after = [ "network.target" ];
+            wantedBy = [ "multi-user.target" ];
+
+            serviceConfig = {
+                Type = "simple";
+                User = cfg.user;
+                Group = cfg.group;
+                WorkingDirectory = cfg.stateDir;
+                Restart = "always";
+                RestartSec = 20;
+            };
+
+            environment = {
+                LABELER_DB_PATH =  "${cfg.stateDir}/feed.sqlite";
+            };
+
+            # this is where we can write a bash script to do everything we need 
+            script = ''
+                exec ${cfg.package}/bin/fanworks-labeling-site
+            '';
+        };
+
+        services.nginx.virtualHosts."${cfg.host}" = {
+            enableACME = true;
+            forceSSL = true;
+        
+            locations."/" = {
+                proxyWebsockets = true;
+                proxyPass = "http://127.0.0.1:${toString cfg.port}";
+            };
+        }; 
+    };
+}
---
+++ packages/x86_64-linux/labeling-site/default.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  writeScriptBin,
+  buildNpmPackage,
+  nodejs_22,
+  makeWrapper,
+  importNpmLock,
+  ...
+}:  let
+    package-json = lib.importJSON (lib.snowfall.fs.get-file "./labeling-site/package.json");
+    labels = lib.snowfall.fs.get-file "./labels.ts";
+in
+  buildNpmPackage {
+    pname = "fanworks-labeling-site";
+	inherit (package-json) version;
+	
+    src = lib.snowfall.fs.get-file "./labeling-site";
+
+    npmDeps = importNpmLock {
+    	npmRoot = lib.snowfall.fs.get-file "./labeling-site";
+    };
+
+    npmConfigHook = importNpmLock.npmConfigHook;
+
+    npmFlags = [ "--ignore-scripts" "--legacy-peer-deps" ];
+
+    nodejs = nodejs_22;
+
+    dontNpmBuild = true;
+
+    nativeBuildInputs = [makeWrapper];
+
+	postUnpack = ''
+		cp --no-preserve=mode ${labels} $sourceRoot/src/labels.ts
+	'';
+
+    postInstall = ''
+      makeWrapper ${nodejs_22}/bin/node $out/bin/fanworks-labeling-site --add-flags $out/lib/node_modules/labeling-site/node_modules/.bin/astro --add-flags "dev"
+    '';
+  }
